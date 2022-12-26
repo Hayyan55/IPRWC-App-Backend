cee32433c4814894f6748b1ae610e43846c3d5dc
Hayyan55 <hayan_mez@hotmail.co.uk>
2022-12-26 14:00:46 +0100

    clean up

--- src/test/java/HogeschoolLeiden/IPRWCApp/product/ProductRepoTest.java
+++ src/test/java/HogeschoolLeiden/IPRWCApp/product/ProductRepoTest.java
@@ -1,34 +1,34 @@
-//package HogeschoolLeiden.IPRWCApp.product;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.annotation.Rollback;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//
-//
-//@WebAppConfiguration
-//@SpringBootTest
-//@Rollback(value = false)
-//class ProductRepoTest {
-//
-//    @Autowired
-//    private ProductRepo productRepo;
-//
-//    @Test
-//    void existsById() {
-//        Product testProduct = Product.builder()
-//                .productName("Hayyan")
-//                .productPrise(123)
-//                .build();
-//        productRepo.existsById(testProduct.getId());
-//    }
-//
-//    @Test
-//    void findProductByProductName() {
-//    }
-//
-//    @Test
-//    void deleteProductByIdAndProductName() {
-//    }
-//}
+package HogeschoolLeiden.IPRWCApp.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@WebAppConfiguration
+@SpringBootTest
+@Rollback(value = false)
+class ProductRepoTest {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Test
+    void existsById() {
+        Product testProduct = Product.builder()
+                .productName("Hayyan")
+                .productPrise(123)
+                .build();
+        productRepo.existsById(testProduct.getId());
+    }
+
+    @Test
+    void findProductByProductName() {
+    }
+
+    @Test
+    void deleteProductByIdAndProductName() {
+    }
+}
