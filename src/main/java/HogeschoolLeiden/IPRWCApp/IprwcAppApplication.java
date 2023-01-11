6574eb5ae903a3debb5303b60b6c230f445edc4f
Hayyan55 <hayan_mez@hotmail.co.uk>
2023-01-11 18:10:19 +0100

    Last Commit

--- src/main/java/HogeschoolLeiden/IPRWCApp/IprwcAppApplication.java
+++ src/main/java/HogeschoolLeiden/IPRWCApp/IprwcAppApplication.java
@@ -20,42 +20,4 @@ public class IprwcAppApplication {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-//	@Autowired
-//	ProductRepo productRepo;
-//	@Bean
-//	CommandLineRunner run(UserService userService, ProductService productService) {
-//		return args -> {
-//			userService.saveRole(new Role(null,USER));
-//			userService.saveRole(new Role(null,ADMIN));
-//			userService.saveRole(new Role(null,SUPER_ADMIN));
-
-//			userService.saveUser(new AppUser(null,"Hayyan Mezher", "Hayyan", "1324", new ArrayList<>()));
-//			userService.saveUser(new AppUser(null,"Tomoka Shoji", "Tomo", "1234", new ArrayList<>()));
-//			userService.saveUser(new AppUser(null,"Haruka Mezher", "Haruka", "4321", new ArrayList<>()));
-
-//			userService.addRoleToUser("Hayyan", ADMIN);
-//			userService.addRoleToUser("Tomo", SUPER_ADMIN);
-//			userService.addRoleToUser("Haruka", USER);
-
-//			Product product = new Product(null, "Karaage1", "", 123, "");
-//			productRepo.save(product);
-//			Product product1 = new Product(null, "Karaage2", "", 123, "");
-//			productRepo.save(product1);
-//			Product product2 = new Product(null, "Karaage3", "", 123, "");
-//			productRepo.save(product2);
-//			Product product3 = new Product(null, "Karaage4", "", 123, "");
-//			productRepo.save(product3);
-//			productService.addProduct(new Product(null, "Karaage", "", 123, ""));
-//			productService.addProduct(new Product(null, "Karaage XL", "", 123, ""));
-//			productService.addProduct(new Product(null, "Curry", "", 123, ""));
-//			productService.addProduct(new Product(null, "Curry Tonkatsu", "", 123, ""));
-//			productService.addProduct(new Product(null, "Curry Karaage", "", 123, ""));
-//			productService.addProduct(new Product(null, "Miso Soup", "", 123, ""));
-//			productService.addProduct(new Product(null, "Rice Burger", "", 123, ""));
-//			productService.addProduct(new Product(null, "Tofu Salad", "", 123, ""));
-//			productService.addProduct(new Product(null, "KaraMiso Set", "", 123, ""));
-//			productService.addProduct(new Product(null, "Onigiri", "", 123, ""));
-//		};
-//	}
 }
